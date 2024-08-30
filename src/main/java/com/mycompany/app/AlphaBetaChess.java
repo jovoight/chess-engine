9df235145f198e02ca21a38ad9d05709a7b8ce93
Josiah Voight <jovoight@gmail.com>
2024-08-30 16:13:22 -0500

    first commit

---
+++ src/main/java/com/mycompany/app/AlphaBetaChess.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+public class AlphaBetaChess {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
