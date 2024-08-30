965592f741d6cff086ac54f99d2cebfd6ad183ae
Josiah Voight <jovoight@gmail.com>
2024-08-30 16:26:49 -0500

    basic GUI box

--- src/main/java/com/mycompany/app/UserInterface.java
+++ src/main/java/com/mycompany/app/UserInterface.java
@@ -1,5 +1,10 @@
 package com.mycompany.app;
+import java.awt.Graphics;
 
-public class UserInterface {
-  
+import javax.swing.JPanel;
+
+public class UserInterface extends JPanel {
+	public void paintComponent(Graphics g) {
+	
+	}
 }
